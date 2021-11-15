70b0790c73a994791b7c0601c1734a2dac6c475d
Hussein Mahameed <hussein.mahameed@symbolab.com>
2021-11-15 11:45:39 +0200

    Initial Commit

---
+++ motionAcceleration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  motionAcceleration
+//
+//  Created by Hussein Mahameed on 15/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
